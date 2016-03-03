c820eed8a9ddb5dd70fb1bcb776d23d4e948ec8a
Shihong Zhong <kydist@hotmail.com>
2016-03-03 10:12:46 -0500

    added the function

---
+++ Myo/Assets/Myo Samples/Scripts/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class button_manager : MonoBehaviour {
+
+	public void OnClick(int SceneName){
+		SceneManager.LoadScene(SceneName);
+	}
+	//public void OnClick(){
+	//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	//}
+}
